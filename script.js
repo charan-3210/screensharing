3caf50b98d6615fe52e3df8bd9ffdf609f7e56b9
charan-3210 <249y1a3210@ksrmce.ac.in>
2026-09-21 11:20:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const createRoomBtn = document.getElementById("createRoomBtn");
+const joinRoomBtn = document.getElementById("joinRoomBtn");
+
+createRoomBtn.addEventListener("click", () => {
+    alert("Room creation will be connected in the next step.");
+});
+
+joinRoomBtn.addEventListener("click", () => {
+    alert("Room joining will be connected in the next step.");
+});
